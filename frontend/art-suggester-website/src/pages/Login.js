c4d19a260904fe1eaf0f639ea1b22b55c632f9b2
Johann235 <johann.caancan@gmail.com>
2024-10-27 22:29:05 -0400

    Starting the login page

--- frontend/art-suggester-website/src/pages/Login.js
+++ frontend/art-suggester-website/src/pages/Login.js
@@ -1,10 +1,25 @@
 import NavBar from "../components/NavBar";
+import './Login.css';
 
 export default function Login() {
     return (
         <>
             <NavBar />
-            <h2>Login Page</h2>
+            <main className="loginPage">
+                <h1>Mac AI Art Suggester</h1>
+                <h2>LOG IN</h2>
+                <div className = "inputs">
+                    <h3 className = "inputLabel">Email</h3>
+                    <input className = "loginInput" placeholder="johndoe@gmail.com"></input>
+                    <h3 className = "inputLabel">Password</h3>
+                    <input className = "loginInput" placeholder="password123"></input>
+                    {/* <!-- TODO: make this centered --> */}
+                    <h3 className = "or">------------OR------------</h3>
+                        <button className = "Google">Login with google</button>
+                    <h3 className = "or">--------------------------</h3>
+                </div>
+                
+            </main>    
         </>
     );
 };

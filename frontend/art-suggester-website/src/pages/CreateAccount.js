2cde85ca000a2401873a045d8c7f5ef01ca7b265
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2024-11-06 22:00:33 -0500

    Template account creation page + styles

--- frontend/art-suggester-website/src/pages/CreateAccount.js
+++ frontend/art-suggester-website/src/pages/CreateAccount.js
@@ -1,10 +1,32 @@
+/*
+ * Aiden Henderson & Johann Caancan
+ */
+
 import NavBar from "../components/NavBar";
+import './CreateAccount.css'
 
 export default function CreateAccount() {
     return (
         <>
             <NavBar />
-            <h2>Account Creation</h2>
+            <main className="createAccount">
+                <h1>Mac AI Art Suggester</h1>
+                <h2>SIGN UP</h2>
+                <div className = "inputs">
+                    <h3 className = "inputLabel">Email</h3>
+                    <input className = "loginInput" placeholder="user@domain.com"></input>
+                    <h3 className = "inputLabel">Password</h3>
+                    <input className = "loginInput" placeholder="password123"></input>
+                    <button className = "Login">Create Account</button>
+                    <div className = 'halfLine'></div> 
+                    <h3 className = "or">OR</h3>
+                    <div className = 'halfLine'></div>
+                    <button className = "Google"><img src="/images/GoogleLogo.png" alt="logo" className='google-logo' />Sign up with google</button>
+                    <div className = 'line'></div>
+                    <p className="noAcc">Already have an account? <a href = '/login'>Log in!</a></p>
+                </div>
+                
+            </main>    
         </>
     );
 };

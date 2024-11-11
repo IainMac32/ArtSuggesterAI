70b05b3680a69c2fe4bf7b7dfbdbd2fe052cb3ea
Johann235 <johann.caancan@gmail.com>
2024-11-10 19:42:34 -0500

    Finished design of favourites page (added packages for icons as well)

--- frontend/art-suggester-website/src/pages/Favourites.js
+++ frontend/art-suggester-website/src/pages/Favourites.js
@@ -4,15 +4,23 @@
 import "./Favourites.css"
 import { useState } from "react";
 import NavBar from "../components/NavBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faDownload, faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function Favourites() {
     const [numFavs, setNumFavs] = useState(0);
     const [favourites, setFavorutites] = useState ([]);
 
     function addFav (){
-        setNumFavs(numFavs + 1)
-        const newFav = [...favourites, numFavs]
-        setFavorutites(newFav)
+        setNumFavs(numFavs + 1);
+        const newFav = [...favourites, numFavs];
+        setFavorutites(newFav);
+    }
+
+    function delFav(toDelIndex){
+        const newFav = favourites.filter((favItem,favIndex) => favIndex !== toDelIndex);
+        setFavorutites (newFav);
+
     }
 
     return (
@@ -24,8 +32,13 @@ export default function Favourites() {
             <ul className = 'favourites'>
             {favourites.map ((favItem, index) => {
                 return (
-                    <div className="favItem">
-                        <li key = {index}>{favItem}</li>
+                    <div className="favItem" key = {index}>
+                        {/*<li>{favItem}</li>*/}
+                        <img src="/images/logo.png" alt="logo" className="img"/>
+                        <div className="icons">
+                            <FontAwesomeIcon icon={faDownload} className="download"/>
+                            <FontAwesomeIcon icon={faStar} className="star" onClick={() => {delFav(index)}}/>
+                        </div>
                     </div>
                 )
             }
